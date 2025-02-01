599287c86810f7acbc12f4da34fc59c80b12e7ef
Hemanta Sharma <hsharma@barracuda.com>
2025-02-01 18:45:40 -0500

    changes

--- script.js
+++ script.js
@@ -17,20 +17,44 @@ function updateTime() {
 setInterval(updateTime, 1000);
 updateTime();
 
-function updateClock() {
+function openTab(evt, tabName) {
+    const tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    const tablinks = document.getElementsByClassName("tablink");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function updateClock(clockId, timeZone) {
     const now = new Date();
-    const seconds = now.getSeconds();
-    const minutes = now.getMinutes();
-    const hours = now.getHours();
+    const options = { timeZone: timeZone, hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+    const timeString = now.toLocaleTimeString('en-US', options);
+    const [hours, minutes, seconds] = timeString.split(':').map(Number);
 
     const secondsDeg = ((seconds / 60) * 360) + 90;
     const minutesDeg = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
     const hoursDeg = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
 
-    document.querySelector('.second-hand').style.transform = `rotate(${secondsDeg}deg)`;
-    document.querySelector('.minute-hand').style.transform = `rotate(${minutesDeg}deg)`;
-    document.querySelector('.hour-hand').style.transform = `rotate(${hoursDeg}deg)`;
+    const clock = document.getElementById(clockId);
+    clock.querySelector('.second-hand').style.transform = `rotate(${secondsDeg}deg)`;
+    clock.querySelector('.minute-hand').style.transform = `rotate(${minutesDeg}deg)`;
+    clock.querySelector('.hour-hand').style.transform = `rotate(${hoursDeg}deg)`;
 }
 
-setInterval(updateClock, 1000);
-updateClock();
+function updateClocks() {
+    updateClock('clock-ny', 'America/New_York');
+    updateClock('clock-london', 'Europe/London');
+    updateClock('clock-kathmandu', 'Asia/Kathmandu');
+}
+
+setInterval(updateClocks, 1000);
+updateClocks();
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.tablink').click();
+});
