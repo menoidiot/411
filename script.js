206db083cf3fe30338b93b691fb59d6a7f4fbf1c
Hemanta Sharma <hsharma@barracuda.com>
2025-02-01 18:42:19 -0500

    analog

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 updateTime();
+
+function updateClock() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const secondsDeg = ((seconds / 60) * 360) + 90;
+    const minutesDeg = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    const hoursDeg = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+
+    document.querySelector('.second-hand').style.transform = `rotate(${secondsDeg}deg)`;
+    document.querySelector('.minute-hand').style.transform = `rotate(${minutesDeg}deg)`;
+    document.querySelector('.hour-hand').style.transform = `rotate(${hoursDeg}deg)`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
