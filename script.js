1b2f169ef5a9e60a31c882ff9974053b335424ef
Hemanta Sharma <hsharma@barracuda.com>
2025-02-01 18:40:35 -0500

    flashy

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ function updateTime() {
 
     const nyTime = now.toLocaleString("en-US", { timeZone: "America/New_York" });
     document.getElementById("time-ny").textContent = nyTime;
+    document.getElementById("time-ny").classList.add("flashy");
 
     const londonTime = now.toLocaleString("en-GB", { timeZone: "Europe/London" });
     document.getElementById("time-london").textContent = londonTime;
+    document.getElementById("time-london").classList.add("flashy");
 
     const ktmTime = now.toLocaleString("en-NP", { timeZone: "Asia/Kathmandu" });
     document.getElementById("time-kathmandu").textContent = ktmTime;
+    document.getElementById("time-kathmandu").classList.add("flashy");
 }
 
 setInterval(updateTime, 1000);
